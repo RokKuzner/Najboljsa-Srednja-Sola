36a772334517ebfeb6c630a7604e4954f773e472
Rok Kuzner <rok.ankaran@gmail.com>
2025-07-24 13:59:32 +0200

    Add initial implementation of Selenium web driver

---
+++ Extract data/extract_24_ur_posts.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import json
+
+# Setup driver for background use
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+
+# Create driver
+driver = webdriver.Chrome(options=chrome_options)
+
+try:
+    driver.get("")
+except Exception as e:
+    print("Exception:", e)
+finally:
+    driver.quit()

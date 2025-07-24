3548767639434816a5116f764c9c110dc3240f6d
Rok Kuzner <rok.ankaran@gmail.com>
2025-07-24 13:42:09 +0200

    Implement headless browser setup

---
+++ Extract data/extract_scores.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+# Setup driver for background use
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+
+# Create driver
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.almamater.si/lestvicasol-srednjesole-s181")
+
+driver.quit()

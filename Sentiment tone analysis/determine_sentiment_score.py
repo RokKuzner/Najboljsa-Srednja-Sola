423030bfc5c2bf4c50acb352fc2cb95ab0a50706
Rok Kuzner <rok.ankaran@gmail.com>
2025-07-26 17:17:09 +0200

    calculate sentiment scores

--- Sentiment tone analysis/determine_sentiment_score.py
+++ Sentiment tone analysis/determine_sentiment_score.py
@@ -4,3 +4,15 @@ import json
 # Get school data
 with open("schools.json", "r") as f:
     schools = json.load(f)
+
+# Calculate scores
+for indx, school in enumerate(schools):
+    scores:list[float] = []
+
+    for article in school["articles"]:
+        scores.append( determine_polarity( translate( article) ) )
+
+    score = sum(scores) / len(scores)
+
+    # Save score
+    schools[indx]["sentiment_score"] = score

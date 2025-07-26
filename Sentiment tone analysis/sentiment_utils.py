54446abfc09609ec8da5ab9eefc4ecfb8bea6025
Rok Kuzner <rok.ankaran@gmail.com>
2025-07-26 17:08:17 +0200

    implement translation functionality in sentiment_utils.py

--- Sentiment tone analysis/sentiment_utils.py
+++ Sentiment tone analysis/sentiment_utils.py
@@ -1,8 +1,11 @@
 from textblob import TextBlob
+from deep_translator import GoogleTranslator
+
+translator = GoogleTranslator(source='sl', target='en')
 
 def determine_polarity(text:str) -> float:
     polarity:float = TextBlob(text).sentiment.polarity  # type: ignore
     return polarity
 
 def translate(text:str):
-    pass
+    return translator.translate(text)
